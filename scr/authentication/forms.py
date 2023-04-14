b035eccd03befd712a44dbd11b4e62aa10bbe93b
NoCapAAAAA <arttyyom@yandex.ru>
2023-04-15 00:58:08 +0300

    Initial commit

---
+++ scr/authentication/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+user_model = get_user_model()
+
+
+class RegisterForm(UserCreationForm):
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = user_model
+        fields = ('username', 'email', 'first_name', 'last_name', 'middle_name', 'gender', 'phone_number', 'password1', 'password2')
+
+
+class CustomUserCreationForm(UserCreationForm):
+    # todo check if I need this form
+    class Meta:
+        model = user_model
+        fields = ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    # todo
+    class Meta:
+        model = user_model
+        fields = ('email',)
